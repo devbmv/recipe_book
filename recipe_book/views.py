126d0af51b3a7d285ad2542d36e15ed32af6210b
Mihail <72050084+devbmv@users.noreply.github.com>
2024-07-09 20:54:26 +0100

    Add comment to post

--- recipe_book/views.py
+++ recipe_book/views.py
@@ -1,26 +1,42 @@
 from django.views import generic
-from .models import Post, Comment
+from django.contrib import messages
+from .models import Post
+from .forms import CommentForm
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView
-from django.http import HttpResponse
-
 
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "recipe_book/index.html"
     paginate_by = 4
-    
+
 
 def recipe_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+        messages.add_message(
+            request, messages.SUCCESS, "Comment submitted and awaiting approval"
+        )
+    comment_form = CommentForm()
 
     return render(
         request,
         "recipe_book/recipe_detail.html",
-        {"post": post,
-        "coder":"Matt Rudge",
-        }
+        {
+            "post": post,
+            "coder": "Mick",
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
     )
