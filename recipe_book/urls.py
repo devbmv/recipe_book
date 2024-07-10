455d039bed745235c7c713767fb80db667f7eb47
Mihail <72050084+devbmv@users.noreply.github.com>
2024-07-10 16:16:09 +0100

    Create crud functionality

--- recipe_book/urls.py
+++ recipe_book/urls.py
@@ -4,6 +4,16 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path("", PostList.as_view(), name="home"),
+    path("<slug:slug>/", views.recipe_detail, name="recipe_detail"),
+    path(
+        "<slug:slug>/edit_comment/<int:comment_id>",
+        views.comment_edit,
+        name="comment_edit",
+    ),
+    path(
+        "<slug:slug>/delete_comment/<int:comment_id>",
+        views.comment_delete,
+        name="comment_delete",
+    ),
 ]
--- recipe_book/views.py
+++ recipe_book/views.py
@@ -1,8 +1,9 @@
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 
 
 class PostList(generic.ListView):
@@ -33,7 +34,7 @@ def recipe_detail(request, slug):
             comment_form = CommentForm()  # Redefine comment_form to clear the form
     else:
         comment_form = CommentForm()
-    print("About to render template")
+        print("Resetare variabila commment form")
     return render(
         request,
         "recipe_book/recipe_detail.html",
@@ -45,3 +46,43 @@ def recipe_detail(request, slug):
             "new_comment": new_comment,
         },
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, "Comment Updated!")
+        else:
+            messages.add_message(request, messages.ERROR, "Error updating comment!")
+
+    return HttpResponseRedirect(reverse("recipe_detail", args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
